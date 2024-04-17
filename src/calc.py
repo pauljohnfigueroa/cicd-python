705b16e145c57be09edea157f96c87bfe3d0d19b
Paul Figueroa <pjsfigueroa@gmail.com>
2024-04-17 13:10:02 +0800

    Add autopep8

--- src/calc.py
+++ src/calc.py
@@ -1,6 +1,6 @@
 def add_two_nums(a, b):
     return a + b
 
+
 def subtract_two_nums(a, b):
     return a - b
-
--- src/numpy.py
+++ src/numpy.py
@@ -1,4 +1,3 @@
 import src.numpy as np
 arr = np.array([1, 2, 3, 4, 5])
 print(arr)
-
